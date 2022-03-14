2edff3c32062f42aebbad60175a5d5ff30d51930
AlexandrKushch <a.kushch9@gmail.com>
2022-03-14 16:01:53 +0200

    first step

---
+++ src/main/java/com/khpi/vragu/GreetingController.java
@@ -0,0 +1,16 @@
+package com.khpi.vragu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/")
+    public String showGreeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+                               Model model) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+}
